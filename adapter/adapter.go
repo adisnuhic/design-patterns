3e4d6b544412f8398946aea4a5c38ccc4d6d5d7e
adisnuhic <athis89@gmail.com>
2023-08-01 21:49:15 +0200

    implemented adapter pattern

---
+++ adapter/adapter.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+)
+
+// PaymentGateway is the common interface expected by the payment application.
+type PaymentGateway interface {
+	ProcessPayment(amount float64) error
+}
+
+// PayPalGateway is the PayPal payment gateway with its specific API.
+type PayPalGateway struct{}
+
+// MakePayment is the method for processing payment using PayPal's API.
+func (pg *PayPalGateway) MakePayment(amount float64) error {
+	// Simulate processing payment with PayPal.
+	// In a real implementation, it would call the actual PayPal API.
+	fmt.Printf("Processing payment of $%.2f with PayPal\n", amount)
+	return nil
+}
+
+// StripeGateway is the Stripe payment gateway with its specific API.
+type StripeGateway struct{}
+
+// Charge is the method for processing payment using Stripe's API.
+func (sg *StripeGateway) Charge(amount float64) error {
+	// Simulate processing payment with Stripe.
+	// In a real implementation, it would call the actual Stripe API.
+	fmt.Printf("Charging payment of $%.2f with Stripe\n", amount)
+	return nil
+}
+
+// PayPalAdapter adapts PayPalGateway to the common PaymentGateway interface.
+type PayPalAdapter struct {
+	PayPal *PayPalGateway
+}
+
+// ProcessPayment implements the common PaymentGateway interface using PayPalGateway's API.
+func (pa *PayPalAdapter) ProcessPayment(amount float64) error {
+	if pa.PayPal == nil {
+		pa.PayPal = &PayPalGateway{}
+	}
+	return pa.PayPal.MakePayment(amount)
+}
+
+// StripeAdapter adapts StripeGateway to the common PaymentGateway interface.
+type StripeAdapter struct {
+	Stripe *StripeGateway
+}
+
+// ProcessPayment implements the common PaymentGateway interface using StripeGateway's API.
+func (sa *StripeAdapter) ProcessPayment(amount float64) error {
+	if sa.Stripe == nil {
+		sa.Stripe = &StripeGateway{}
+	}
+	return sa.Stripe.Charge(amount)
+}
+
+func main() {
+	// Create the payment application using the common PaymentGateway interface.
+	paymentApp := &PaymentApplication{}
+
+	// Use PayPal via the PayPalAdapter.
+	paymentApp.SetPaymentGateway(&PayPalAdapter{})
+	paymentApp.ProcessPayment(100.50)
+
+	// Use Stripe via the StripeAdapter.
+	paymentApp.SetPaymentGateway(&StripeAdapter{})
+	paymentApp.ProcessPayment(50.75)
+}
+
+// PaymentApplication is the payment application.
+type PaymentApplication struct {
+	paymentGateway PaymentGateway
+}
+
+// SetPaymentGateway sets the payment gateway to be used by the application.
+func (app *PaymentApplication) SetPaymentGateway(pg PaymentGateway) {
+	app.paymentGateway = pg
+}
+
+// ProcessPayment processes the payment for the specified amount.
+func (app *PaymentApplication) ProcessPayment(amount float64) {
+	if app.paymentGateway == nil {
+		fmt.Println("Payment gateway not set")
+		return
+	}
+	err := app.paymentGateway.ProcessPayment(amount)
+	if err != nil {
+		fmt.Println("Payment failed:", err)
+	}
+}
