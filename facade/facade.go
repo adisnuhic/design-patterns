d2664c706c049d794767522e8ea749e7389bcf87
adisnuhic <athis89@gmail.com>
2023-08-02 15:10:54 +0200

    implemented facade pattern

---
+++ facade/facade.go
@@ -0,0 +1,77 @@
+package main
+
+import "fmt"
+
+// FlightBookingSubsystem represents the flight booking system with its own set of components and operations.
+type FlightBookingSubsystem struct{}
+
+func (f *FlightBookingSubsystem) SearchFlights(destination, departureDate, returnDate string) []string {
+	// Simulate searching for available flights based on the provided criteria.
+	// In a real implementation, it would call the actual flight booking API.
+	return []string{"Flight 1", "Flight 2", "Flight 3"}
+}
+
+func (f *FlightBookingSubsystem) ReserveFlight(flightID string) error {
+	// Simulate reserving a flight.
+	// In a real implementation, it would call the actual flight booking API.
+	fmt.Printf("Reserved Flight: %s\n", flightID)
+	return nil
+}
+
+// HotelBookingSubsystem represents the hotel booking system with its own set of components and operations.
+type HotelBookingSubsystem struct{}
+
+func (h *HotelBookingSubsystem) SearchHotels(destination, checkInDate, checkOutDate string) []string {
+	// Simulate searching for available hotels based on the provided criteria.
+	// In a real implementation, it would call the actual hotel booking API.
+	return []string{"Hotel A", "Hotel B", "Hotel C"}
+}
+
+func (h *HotelBookingSubsystem) ReserveHotel(hotelName string) error {
+	// Simulate reserving a hotel.
+	// In a real implementation, it would call the actual hotel booking API.
+	fmt.Printf("Reserved Hotel: %s\n", hotelName)
+	return nil
+}
+
+// TravelBookingFacade provides a simplified interface to the complex travel booking subsystems.
+type TravelBookingFacade struct {
+	flightSubsystem *FlightBookingSubsystem
+	hotelSubsystem  *HotelBookingSubsystem
+}
+
+func NewTravelBookingFacade() *TravelBookingFacade {
+	return &TravelBookingFacade{
+		flightSubsystem: &FlightBookingSubsystem{},
+		hotelSubsystem:  &HotelBookingSubsystem{},
+	}
+}
+
+// BookTravelPackage is the simplified interface that wraps the complex operations of flight and hotel booking.
+func (f *TravelBookingFacade) BookTravelPackage(destination, departureDate, returnDate, checkInDate, checkOutDate string) {
+	fmt.Println("Searching for available flights...")
+	flights := f.flightSubsystem.SearchFlights(destination, departureDate, returnDate)
+	if len(flights) > 0 {
+		f.flightSubsystem.ReserveFlight(flights[0])
+		fmt.Println("Flight reservation successful!")
+	} else {
+		fmt.Println("No flights available for the selected dates.")
+	}
+
+	fmt.Println("Searching for available hotels...")
+	hotels := f.hotelSubsystem.SearchHotels(destination, checkInDate, checkOutDate)
+	if len(hotels) > 0 {
+		f.hotelSubsystem.ReserveHotel(hotels[0])
+		fmt.Println("Hotel reservation successful!")
+	} else {
+		fmt.Println("No hotels available for the selected dates.")
+	}
+
+	fmt.Println("Travel package booking complete!")
+}
+
+func main() {
+	// Client code interacts with the TravelBookingFacade to book a complete travel package.
+	travelFacade := NewTravelBookingFacade()
+	travelFacade.BookTravelPackage("New York", "2023-08-15", "2023-08-25", "2023-08-15", "2023-08-25")
+}
